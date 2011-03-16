40d2a33cfce392353cd81b91cf4cbc5439ef2642
Spencer Judd <scj@foxrunn.net>
2011-03-16 15:01:54 -0400

    initial commit

---
+++ surveymonkey.rb
@@ -0,0 +1,66 @@
+#!/usr/bin/env ruby
+require 'mechanize'
+
+class SurveyMonkey
+  attr_reader :surveys
+  
+  def initialize(username, password)
+    @username, @password = username, password
+    @agent = Mechanize.new
+    login
+  end
+  
+  def get_surveys(filter = /.*/)
+    # Scrape index pages
+    @agent.get "http://www.surveymonkey.com/MySurveys.aspx"
+    @surveys = Array.new
+    scrape_index(filter)
+    @agent.page.links_with(:text => /^\d+$/).each do |page_link|
+      page_link.click
+      scrape_index(filter)
+    end
+    
+    # Scrape detail pages
+    @surveys.collect!.with_index do |s, i|
+      puts "Scraping #{i+1}/#{@surveys.length} (#{(i+1)*100/@surveys.length}%)"
+      Survey.new(s)
+    end
+  end
+  
+  private
+  def login
+    @agent.get "http://www.surveymonkey.com/MyAccount_Login.aspx"
+    @agent.page.form_with(:name => "frmLogin").tap do |login|
+      login.field_with(:name => "wc_Login1$txtUsername").value = @username
+      login.field_with(:name => "wc_Login1$txtPassword").value = @password
+      login.add_field!("__EVENTTARGET", "wc_Login1$btnLogin")
+    end.submit
+  end
+  
+  def scrape_index(filter)
+    @agent.page.links_with(:text => filter).each { |link| @surveys << link }
+  end
+end
+
+class Survey
+  attr_reader :name, :completed
+  
+  def initialize(link)
+    @link = link
+    scrape_detail
+  end
+  
+  private
+  def scrape_detail
+    results = @link.click.link_with(:href => /_Responses.aspx/).click.link_with(:text => /view all pages/).click
+    @name = @link.text  # Name
+    @completed = results.search('//div[@id="panTotals"]/table/tr/td[2]/b').text   # Total Completed
+  end
+end
+
+sm = SurveyMonkey.new("username", "password")
+surveys = sm.get_surveys(/EWomen/)
+surveys.each do |s|
+  puts "--- #{s.name} ---"
+  puts "# Completed: #{s.completed}"
+end
