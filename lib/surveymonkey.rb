5fb051f030ef535f3277ca6eb4236e9327f7c681
Spencer Judd <scj@foxrunn.net>
2011-03-16 15:38:28 -0400

    added 2 basic exceptions

--- lib/surveymonkey.rb
+++ lib/surveymonkey.rb
@@ -1,6 +1,19 @@
 require 'mechanize'
 
 module SurveyMonkey
+  
+  # All exceptions raised are a subclass of this class.
+  # Allows one to catch SurveyMonkey::Error
+  class Error < Exception
+  end
+
+  # This exception is raised when login fails
+  class LoginError < Error
+    def initialize
+      super "Login failed. Check username and password."
+    end
+  end
+  
   autoload :Account, "surveymonkey/account"
   autoload :Survey,  "surveymonkey/survey"
 end
