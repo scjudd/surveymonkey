eda01434f1e8240e3a8fc03f20a817b9b8e8c030
Spencer Judd <scj@foxrunn.net>
2011-03-22 23:18:39 -0400

    implemented SurveyMonkey::LoginError

--- lib/surveymonkey/account.rb
+++ lib/surveymonkey/account.rb
@@ -36,6 +36,7 @@ module SurveyMonkey
         login.field_with(:name => "wc_Login1$txtPassword").value = password
         login.add_field!("__EVENTTARGET", "wc_Login1$btnLogin")
       end.submit
+      raise SurveyMonkey::LoginError if @agent.page.body.match('Authentication failed')
     end
     
     def scrape_index(filter)
